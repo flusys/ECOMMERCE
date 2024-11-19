5bf5b0a79e4a5f685b33c31b2b0725a2fc506a53
sfd-mhrana <sfd.mhrana@gmail.com>
2024-11-19 22:42:46 +0600

    Product and parent product shema

--- apps/ECOMMERCE-API/src/app/modules/product/parent-product.schema.ts
+++ apps/ECOMMERCE-API/src/app/modules/product/parent-product.schema.ts
@@ -1,24 +1,117 @@
+import { Schema } from 'mongoose';
 import * as mongoose from 'mongoose';
 
-export const ParentProductSchema = new mongoose.Schema(
+const ParentProductSchema = new mongoose.Schema(
   {
     readOnly: {
       type: Boolean,
-      required: false,
+      required: true,
+      default: false,
     },
     name: {
       type: String,
       required: true,
-      trim: true,
     },
-    priority: {
-      type: Number,
-      required: false,
-    },
-    image: {
+    slug: {
       type: String,
       required: true,
-      trim: true,
+      unique: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    description: {
+      type: String,
+      default: null,
+    },
+    isHtml: {
+      type: Boolean,
+      default: null,
+    },
+    serial: {
+      type: Number,
+      default: null,
+    },
+    shortDesc: {
+      type: String,
+      default: null,
+    },
+    seoTitle: {
+      type: String,
+      default: null,
+    },
+    seoDescription: {
+      type: String,
+      default: null,
+    },
+    seoKeywords: [
+      {
+        type: String,
+      },
+    ],
+    videoUrl: {
+      type: String,
+      default: null,
+    },
+    videoThumbnailImage: {
+      type: String,
+      default: null,
+    },
+    specifications: [
+      {
+        key: String,
+        value: String,
+      },
+    ],
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category',
+      default: null,
+    },
+    brand: {
+      type: Schema.Types.ObjectId,
+      ref: 'Brand',
+      default: null,
+    },
+    company: {
+      type: Schema.Types.ObjectId,
+      ref: 'Company',
+      default: null,
+    },
+    tags: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Tag',
+      },
+    ],
+    products: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Product',
+      },
+    ],
+    isFeature: {
+      type: Boolean,
+      default: null,
+    },
+    status: {
+      type: String,
+      default: null,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    createdAtString: {
+      type: String,
+      required: false,
+    },
+    updatedAtString: {
+      type: String,
+      required: false,
     },
   },
   {
---
+++ apps/ECOMMERCE-API/src/app/modules/product/product.schema.ts
@@ -0,0 +1,109 @@
+import { Schema } from 'mongoose';
+import * as mongoose from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    readOnly: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    image: {
+      type: String,
+      default: null,
+    },
+    warning: {
+      type: String,
+      default: null,
+    },
+    warningDay: {
+      type: Number,
+      default: null,
+    },
+    refundable: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    returnable: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    sku: {
+      type: String,
+      default: null,
+    },
+    barCode: {
+      type: String,
+      unique: true,
+      default: null,
+    },
+    price: {
+      type: Number,
+      default: null,
+    },
+    ingredients: [
+      {
+        key: String,
+        value: String,
+      },
+    ],
+    trackQuantity: {
+      type: Boolean,
+      default: null,
+    },
+    earnPoint: {
+      type: Number,
+      default: null,
+    },
+    isActive: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    activeOnline: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+    status: {
+      type: String,
+      default: null,
+    },
+    parentInfo: {
+      type: Schema.Types.ObjectId,
+      ref: 'ParentProduct',
+      required: true,
+    },
+    variants: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'ProductSubVariant',
+      },
+    ],
+    variantIds: [
+      {
+        type: Number,
+      },
+    ],
+    purchases: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'ProductPurchases',
+      },
+    ],
+    createdAtString: {
+      type: String,
+      required: false,
+    },
+    updatedAtString: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  },
+);
