3412c8034b866e7a227016d99d1cc42322e696cd
sfd-mhrana <sfd.mhrana@gmail.com>
2024-12-09 08:50:06 +0600

    Some changes

--- libs/shared/src/modules/prime.module.ts
+++ libs/shared/src/modules/prime.module.ts
@@ -7,6 +7,7 @@ import { InputSwitchModule } from "primeng/inputswitch";
 import { PasswordModule } from "primeng/password";
 import { ButtonModule } from "primeng/button";
 import { TooltipModule } from "primeng/tooltip";
+import { DynamicDialog } from 'primeng/dynamicdialog';
 import { CheckboxModule } from "primeng/checkbox";
 import { StepsModule } from "primeng/steps";
 import { RippleModule } from "primeng/ripple";
@@ -14,17 +15,12 @@ import { PanelModule } from 'primeng/panel';
 import { PaginatorModule } from 'primeng/paginator';
 import { TableModule } from 'primeng/table';
 import { InputNumberModule } from "primeng/inputnumber";
-import { Textarea } from "primeng/inputtextarea";
+import { TextareaModule } from 'primeng/textarea';
 import { ProgressBarModule } from "primeng/progressbar";
 import { FileUploadModule } from 'primeng/fileupload';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { SidebarModule } from 'primeng/sidebar';
-import { RadioButtonModule } from 'primeng/radiobutton';
 import { ListboxModule } from 'primeng/listbox';
-import {ToggleButtonModule} from "primeng/togglebutton";
-import { OrganizationChartModule } from 'primeng/organizationchart';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
+
 @NgModule({
   exports: [
     InputTextModule,
@@ -35,6 +31,7 @@ import { InputIconModule } from 'primeng/inputicon';
     PasswordModule,
     ButtonModule,
     TooltipModule,
+    DynamicDialog,
     CheckboxModule,
     StepsModule,
     RippleModule,
@@ -42,17 +39,11 @@ import { InputIconModule } from 'primeng/inputicon';
     PaginatorModule,
     TableModule,
     InputNumberModule,
-    Textarea,
+    TextareaModule,
     ProgressBarModule,
     FileUploadModule,
     OverlayPanelModule,
-    SidebarModule,
-    RadioButtonModule,
-    ListboxModule,
-    ToggleButtonModule,
-    OrganizationChartModule,
-    IconFieldModule,
-    InputIconModule,
+    ListboxModule
   ]
 })
 export class PrimeModule {
